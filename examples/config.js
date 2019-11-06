c9910ff243375a72dc82a81aa8682b79407cb974
Praveen Kumar <praveen.kumar@appster.in>
2019-11-06 15:31:38 +0530

    on RWD

--- examples/config.js
+++ examples/config.js
@@ -1,10 +1,19 @@
 module.exports = {
   breakPoints: {
+    xs: "@media screen and (max-width: 585px)",
     maxLg: "@media screen and (max-width: 899.99px)",
     lg: "@media screen and (min-width: 900px)"
   },
   custom: {
     headerbg: "url(../img/vwo.jpg)",
-    mobileBgPos: "bottom -240px center"
+    mobileBgPos: "bottom -240px center",
+    borderWidth: "2px",
+    borderColor: "#BDBDBD",
+    inputHtMobile: "52px",
+    inputHtLg: "37px",
+    formLblMargin: "14px",
+    gutter: {
+      default: "20px"
+    }
   }
 };
