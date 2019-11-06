8e9660f80bc56052ea1de71dd73f2000bcc1039d
Praveen Kumar <praveen.kumar@appster.in>
2019-11-06 08:55:19 +0530

    mobile header done

--- examples/config.js
+++ examples/config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    "custom": {
-        "Bgc($primary)": "#f3f3f3"
-    }
+  breakPoints: {
+    maxLg: "@media screen and (max-width: 899.99px)",
+    lg: "@media screen and (min-width: 900px)"
+  },
+  custom: {
+    headerbg: "url(../img/vwo.jpg)",
+    mobileBgPos: "bottom -240px center"
+  }
 };
