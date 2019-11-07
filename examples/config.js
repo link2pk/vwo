614906ac2abde7a98b5244a538261c9c9652ca55
Praveen Kumar <praveen.kumar@appster.in>
2019-11-07 23:13:36 +0530

    v1

--- examples/config.js
+++ examples/config.js
@@ -1,21 +1,26 @@
 module.exports = {
   breakPoints: {
     xs: "@media screen and (max-width: 585px)",
+    sm: "@media screen and (min-width:586px) and (max-width:899px)",
     maxLg: "@media screen and (max-width: 899.99px)",
     lg: "@media screen and (min-width: 900px)"
   },
   custom: {
     headerbg: "url(../img/vwo.jpg)",
+    mainDesktopBoxShadow: "0px 15px 30px rgba(0, 0, 0, 0.19)",
     mobileBgPos: "bottom -241px center",
+    desktopBgPos: "left 210px top 46px",
     borderWidth: "2px",
     borderColor: "#BDBDBD",
     inputPadding: "14px",
     inputHtMobile: "52px",
     inputHtLg: "37px",
-    formLblMargin: "14px",
+    formLblMarginMobile: "14px",
+    formLblMarginDesktop: "5px",
     formColor: "#2D9CDB",
-    gutter: {
-      default: "20px"
+    paddingGutter: {
+      default: "20px",
+      lg: "20px 60px"
     },
     TrsBgc3sEase: "background-color .3s ease"
   }
