75ec642ce058a9039d288e457ad8daf7ad22adea
Praveen Kumar <praveen.kumar@appster.in>
2019-11-09 16:49:34 +0530

    maxLg changed to maxSm

--- html/config.js
+++ html/config.js
@@ -2,7 +2,7 @@ module.exports = {
   breakPoints: {
     xs: "@media screen and (max-width: 585px)",
     sm: "@media screen and (min-width:586px) and (max-width:899px)",
-    maxLg: "@media screen and (max-width: 899.99px)",
+    maxSm: "@media screen and (max-width: 899.99px)",
     lg: "@media screen and (min-width: 900px)"
   },
   custom: {
